4f0e57b3937b9397272bf36bfc30f5f291ac893e
Demo6339 <demosouls6339@gmail.com>
2025-09-18 09:42:12 +0700

    Setup!

--- Client/Assets/Global/Routes/root.jsx
+++ Client/Assets/Global/Routes/root.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Home from '../../../Source/Main/Home/page.jsx'
 import Review from '../../../Source/Main/Review/page.jsx'
+import Home from '../../../Source/Main/Home/page.jsx'
+import Store from '../../../Source/Other/Store/page.jsx'
+import Event from '../../../Source/Other/Event/page.jsx'
+import Forum from '../../../Source/Other/Forum/page.jsx'
 import Profile from '../../../Source/Auth/Profile/page.jsx'
 import Register from '../../../Source/Auth/Register/page.jsx'
 import Recover from '../../../Source/Auth/Recover/page.jsx'
-import Event from '../../../Source/Other/Event/page.jsx'
-import Store from '../../../Source/Other/Store/page.jsx'
 import Legal from '../../Components/Legal/item.jsx'
 import Information from '../../Components/Information/item.jsx'
 import PrivacyTerms from '../../Components/Legal/PrivacyTerms/clause.jsx'
 import TermsOfService from '../../Components/Legal/TermsOfService/clause.jsx'
+import AboutUs from '../../../Source/Other/Helper/AboutUs/page.jsx'
+import Cooperate from '../../../Source/Other/Helper/Cooperate/page.jsx'
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Review />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/review" element={<Review />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/register" element={<Register />} />
@@ -27,6 +31,9 @@ function AppRoutes() {
       <Route path="/legal" element={<Legal />} />
       <Route path="/legal/privacy-terms" element={<PrivacyTerms />} />
       <Route path="/legal/terms-of-service" element={<TermsOfService />} />
+      <Route path="/forum" element={<Forum />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/cooperate" element={<Cooperate />} />
     </Routes>
   )
 }
