d23371ae512434a6cf62ef2f80e888b6928d37c3
Demo6339 <demosouls6339@gmail.com>
2025-09-26 15:15:24 +0700

    Setup file

---
+++ Client/Source/Other/Helper/About/page.jsx
@@ -0,0 +1,4 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../../../../Assets/Global/Styles/root.css'
+import '../../../Assets/Global/Scripts/root.jsx'
---
+++ Server/Routes/routes.jsx
@@ -0,0 +1,4 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../../Client/Assets/Global/Styles/root.css'
+import '../../Client/Assets/Global/Scripts/root.jsx'
