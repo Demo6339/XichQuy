baa43fdf00a5f77341c05680cf15426cd0fd5d72
Demo6339 <demosouls6339@gmail.com>
2025-09-27 07:30:11 +0700

    Setup main.jsx

--- Client/Source/main.jsx
+++ Client/Source/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '../Assets/Global/Scripts/root.jsx'
 import AppRoutes from '../../Server/Routes/routes.jsx'
 import '../Assets/Global/Styles/root.css'
-import '../Assets/Global/Scripts/root.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppRoutes />
